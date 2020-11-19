fca25bbc95208516c835877236ebf0761848b30f
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-19 17:37:59 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
@@ -13,5 +14,28 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency *1000)}ms")
 
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain.',
+                 'It is decidedly so.',
+                 'Without a doubt.',
+                 'Yes - definitely.',
+                 'You may rely on it.',
+                 'As I see it, yes.',
+                 'Most likely.',
+                 'Outlook good.',
+                 'Yes.',
+                 'Signs point to yes.',
+                 'Reply hazy, rey again.',
+                 'Ask again later.',
+                 'better not tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 'Don't count on it.',
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 client.run(botToken)
