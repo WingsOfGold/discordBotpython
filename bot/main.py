6b1c46b129a840132f7504f6aa9d22f30482dff0
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-26 15:44:29 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,26 +2,13 @@ import discord
 import random
 from discord.ext import commands
 
-
-gmMsg = ["Good morning :smile:", "Oh hello! Break a leg :joy:", "Good morning sleephead :upside_down:", "YO! I bought you a cow so you can drink milk!! Although it's a binary cow :cow2:, here's your binary milk :milk:"];
-gmMsgNum = 0;
-
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
 
-client = commands.Bot(command_prefix = '')
+client = commands.Bot(command_prefix = '*')
 
 @client.event
 async def on_ready():
     print("Bot is ready!")
-
-@client.command(aliases=['hi', 'hello', 'Hi', 'Hello', 'Good morning', 'Gm', 'gm', 'Yo', 'yo'])
-async def _doyouknow(ctx):
-    byeMsg = ["See you again daddy :smiling_face_with_3_hearts:", 
-              "Bell rings, ringning bells, senta's telling you! SAY YOUR PRAYERS! Bye:raised_back_of_hand:",
-              "See you homie :woozy_face:",
-              "Till another time :upside_down:",
-              "Pew it up, Die aside, Pie it down, then next time, maybe PewDiePie shall tell you bye :joy:"]
-    await ctx.send(f'{random.choice(byeMsg)}')
 
 @client.command(aliases=['bye', 'cya', 'cu', 'See you later', 'Till we meet', 'Till our path cross'])
 async def _doyouknow(ctx):
@@ -32,6 +19,6 @@ async def _doyouknow(ctx):
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"Pong! {round(client.latency *1000)}ms")
+    await ctx.send(f"Pong? Ah ping: {round(client.latency *1000)}ms")
 
 client.run(botToken)
