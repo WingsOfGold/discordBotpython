504dcadb9d24af68ab8f6a769f43548f300da280
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-26 07:51:37 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -34,28 +34,4 @@ async def _doyouknow(ctx):
 async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency *1000)}ms")
 
-@client.command(aliases=['8ball'])
-async def _8ball(ctx, *, question):
-    responses = ['It is certain.',
-                 'It is decidedly so.',
-                 'Without a doubt.',
-                 'Yes - definitely.',
-                 'You may rely on it.',
-                 'As I see it, yes.',
-                 'Most likely.',
-                 'Outlook good.',
-                 'Yes.',
-                 'Signs point to yes.',
-                 'Reply hazy, rey again.',
-                 'Ask again later.',
-                 'better not tell you now.',
-                 'Cannot predict now.',
-                 'Concentrate and ask again.',
-                 "Don't count on it.",
-                 'My reply is no.',
-                 'My sources say no.',
-                 'Outlook not so good.',
-                 'Very doubtful.']
-    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
-
 client.run(botToken)
