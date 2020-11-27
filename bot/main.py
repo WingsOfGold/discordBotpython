12c7f0ad1dce87e5d36605ea02adf8f5387d58b3
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-27 03:18:26 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,29 +4,54 @@ from discord.ext import commands
 
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
 client = commands.Bot(command_prefix = '*')
-welcomeCategory = False
-welcomeText = False
+
+def getChannel(guild, channelName=None):
+    for channel in guild.channels:
+        if (channel.name == channelName):
+            return channel or False
 
 @client.event
 async def on_ready():
     print("Bot is ready!")
 
+#@client.event
+#async def on_member_join(member):
+#    welcomeText.send(f'{member} has came!')
+
+#@client.event
+#async def on_member_remove(member):
+#    welcomeText.send(f'{member} has left!')
+
+#@client.command()
+#async def svstatson(ctx):
+#    aC = getChannel(ctx.guild, "")
+
+#@client.command()
+#async def svstatsoff(ctx):
+#    ac = getChannel(ctx.guild, "
+                    
 @client.command()
-async def svstats(ctx):
-    ctx.send("A")
-    
+async def wlcOn(ctx):
+    aC = getChannel(ctx.guild, "____________welcome____________")
+    if (not aC):
+        aC1 = await ctx.guild.create_category('____________welcome____________', position=0)
+        await ctx.guild.create_text_channel('Welcome', category=aC1)
+        return True
+    await ctx.guild.create_text_channel('Welcome', category=aC)
+
 @client.command()
-async def wlc(ctx):
-    global welcomeCategory, welcomeText
-    welcomeCategory = await ctx.guild.create_category('Testing', position=1)
-    welcomeText = await ctx.guild.create_text_channel('Welcome', category=welcomeCategory)
-    welcomeCategory = welcomeCategory.id
-    welcomeText = welcomeText.id
-    ctx.send(f"{welcomeCategory}")
+async def wlcOff(ctx):
+    aC = getChannel(ctx.guild, "____________welcome____________")
+    aC1 = getChannel(ctx.guild, "Welcome")
+    if (aC and aC1):
+        aC1.delete()
+        if (len(aC.channels) == 0):
+            aC.delete()
+        
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"Pong? Ah ping: {round(client.latency *1000)}ms {welcomeCategory}")
+    await ctx.send(f"Pong? Ah ping: {round(client.latency *1000)}ms")
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
@@ -44,23 +69,13 @@ async def _8ball(ctx, *, question):
                  'Ask again later.',
                  'better not tell you now.',
                  'Cannot predict now.',
-                 '-_-',
                  'Concentrate and ask again.',
                  "Don't count on it.",
                  'My reply is no.',
                  'My sources say no.',
                  'Outlook not so good.',
+                 '-_-',
                  'Very doubtful.']
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
-
-@client.event
-async def on_member_join(member):
-    if (welcomeText):
-        welcomeText.send(f'{member} has came!')
-
-@client.event
-async def on_member_remove(member):
-    if (welcomeText):
-        welcomeText.send (f'{member} has left!')
 
 client.run(botToken)
