cf2857682e4ca1197e372003d2543433d8fdd3e1
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-27 11:43:01 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,28 +1,13 @@
 import discord
-#import exports as ex
+import exports as ex
 import random
 from discord.ext import commands
 
 wlcCategoryName = "____________welcome____________"
 wlcTextChannelName = "Welcome :wave:"
-
+*ping
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
 client = commands.Bot(command_prefix = '*')
-
-def getChannel(channels, name):
-    for channel in channels:
-        if (channel.name == name):
-            return channel
-    return False
-
-async def channelCreate(type, guild, name, _overwrites=None, _category=None, _position=0, _topic=None, _slowmode_delay=None, _nsfw=False, _reason=None):
-    if (type == "txt"):
-        channel = await guild.create_text_channel(name, overwrites=_overwrites, category=_category, position=_position, topic=_topic, slowmode_delay=_slowmode_delay, nsfw=_nsfw, reason=_reason)
-    elif (type == "vc"):
-        channel = await guild.create_voice_channel(name, overwrites=_overwrites, category=_category, position=_position, topic=_topic, slowmode_delay=_slowmode_delay, nsfw=_nsfw, reason=_reason)
-    else:
-        channel = await guild.create_category(name, overwrites=_overwrites, category=_category, position=_position, topic=_topic, slowmode_delay=_slowmode_delay, nsfw=_nsfw, reason=_reason)
-    return True
 
 @client.event
 async def on_ready():
