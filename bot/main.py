f99adc54ede83d8420aae44698036b8b8b861707
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-27 11:23:01 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix = '*')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.CustomActivity(name="a game"))
+    await client.change_presence(activity=discord.streaming(name=", watching you snitching"))
     print("Bot is ready!")
                     
 @client.command()
