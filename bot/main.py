fc0cbc6fb1afd617bcbf96e1de5bf36ae5f99e03
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-27 11:40:14 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -15,7 +15,7 @@ def getChannel(channels, name):
             return channel
     return False
 
-def channelCreate(type, guild, name, _overwrites=None, _category=None, _position=0, _topic=None, _slowmode_delay=None, _nsfw=False, _reason=None):
+async def channelCreate(type, guild, name, _overwrites=None, _category=None, _position=0, _topic=None, _slowmode_delay=None, _nsfw=False, _reason=None):
     if (type == "txt"):
         channel = await guild.create_text_channel(name, overwrites=_overwrites, category=_category, position=_position, topic=_topic, slowmode_delay=_slowmode_delay, nsfw=_nsfw, reason=_reason)
     elif (type == "vc"):
