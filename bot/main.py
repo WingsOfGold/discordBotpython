68e3eb4b3800f4f30233873af87d72493b9f2e16
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-27 11:00:44 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix = '*')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('Playing with diamonds'))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('with diamonds'))
     print("Bot is ready!")
                     
 @client.command()
