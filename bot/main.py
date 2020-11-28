8514f8726fe954ab87422405d9074f5754ee6aa2
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-28 10:06:16 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -33,56 +33,9 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f"Pong? Ah ping: {round(client.latency *1000)}ms")
 
-# Server Stats Code:
 @client.command()
-async def svsCtgOn(ctx):
-    svsCategoryI = getChannel(ctx.guild, svsCategoryName)
-    if (svsCategoryI):
-        await ctx.send("It's already on!")
-    else:
-        svsCategory = await ctx.guild.create_category(svsCategoryName, position=svsCtgPos)
-        await ctx.guild.create_voice_channel(svsTotalName, category=svsCategory)
-        await ctx.guild.create_voice_channel(svsMembersName, category=svsCategory)
-        await ctx.guild.create_voice_channel(svsBotsName, category=svsCategory)
-        await ctx.send("Category has been initiated!")
-
-@client.command()
-async def svsCtgOff(ctx):
-    svsCategory = getChannel(ctx.guild, svsCategoryName)
-    if (not svsCategory):
-        await ctx.send("It's not on!")
-        return True
-    for channel in svsCategory.channels:
-        await channel.delete()
-    await svsCategory.delete()
-    await ctx.send("Category has been obliterated!")
-
-# Welcome Category Code:
-@client.command()
-async def wlcCtgOn(ctx):
-    wlcCategoryI = getChannel(ctx.guild, wlcCategoryName)
-    svsCategoryI = getChannel(ctx.guild, svsCategoryName)
-    if (wlcCategoryI):
-        await ctx.send("It's already on!")
-    else:
-        num = wlcCtgPos - 1
-        if (svsCategoryI):
-            num += 1
-        wlcCategory = await ctx.guild.create_category(wlcCategoryName, position=svsCtgPos)
-        await ctx.guild.create_text_channel(wlcRulesName, category=wlcCategory)
-        await ctx.guild.create_text_channel(wlcAnouncementsName, category=wlcCategory)
-        await ctx.send("Category has been initiated!")
-
-@client.command()
-async def wlcCtgOff(ctx):
-    wlcCategoryI = getChannel(ctx.guild, wlcCategoryName)
-    if (not wlcCategoryI):
-        await ctx.send("It's not on!")
-        return True
-    for channel in wlcCategoryI.channels:
-        await channel.delete()
-    await wlcCategoryI.delete()
-    await ctx.send("Category has been obliterated!")
+async def start(ctx):
+    await ctx.guild.create_category("TEST", position=0)
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
