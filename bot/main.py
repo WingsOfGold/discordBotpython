b66efffe88d5df2139da06ab47ccc8b5d45be18d
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-11-29 09:01:51 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -70,6 +70,7 @@ async def svsCtgOff(ctx):
 @client.event
 async def on_guild_channel_create(c):
     if (c.name.find(svsTotalName)):
+        await client.get_channel(782480148011679844).send("test")
         updateSvsStats(c.guild)
 
 #@client.event
