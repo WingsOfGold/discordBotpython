a853a6a606ab633c0fbdee281cb3c2a2c4f2ee19
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-12-03 12:16:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-#import exports.test as ex
+import exports.test as ex
 
 client = commands.Bot(command_prefix='*')#, intents=intents)
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
