01b9a39530cda715038deb88dd38f46bbe410f78
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-12-03 12:41:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import sys
-sys.path.append("./exports")
-
 import discord
 from discord.ext import commands
+
 import test
 
 client = commands.Bot(command_prefix='*')#, intents=intents)
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
-
 
 @client.command()
 async def ping(ctx):
