fa265c8c98a91124bad53d27c17e9cda6f2711a2
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-12-03 13:28:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,63 @@
+import random
 import discord
 from discord.ext import commands
+from exports import channels
 
-from exports import test
+intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
+intents.bans = True
+intents.emojis = True
+intents.integrations = True
+intents.webhooks = True
+intents.invites = True
+intents.voice_states = True
+intents.presences = True
+intents.messages = True
+intents.guild_messages = True
+intents.dm_messages = True
+intents.reactions = True
+intents.guild_reactions = True
+intents.dm_reactions = True
+intents.typing = True
+intents.guild_typing = True
+intents.dm_typing = True
+client = commands.Bot(command_prefix='*', intents=intents)
 
-client = commands.Bot(command_prefix='*')#, intents=intents)
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Pong? Ah! Ping: {round(client.latency *1000)}ms")
 
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain.',
+                 'It is decidedly so.',
+                 'Without a doubt.',
+                 'Yes - definitely.',
+                 'You may rely on it.',
+                 'As I see it, yes.',
+                 'Most likely.',
+                 'Outlook good.',
+                 'Yes.',
+                 'Signs point to yes.',
+                 'Reply hazy, rey again.',
+                 'Ask again later.',
+                 'better not tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 "Don't count on it.",
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 '-_-',
+                 'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="u snitching"))
     await client.get_channel(782480148011679844).send("I'm ready!")
-    await client.get_channel(782480148011679844).send(test.re())
 
 client.run(botToken)
