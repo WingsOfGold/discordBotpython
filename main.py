05e5aae740b0c20c6bed2f878fbbea6ac7f69837
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-12-03 12:07:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import discord
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
 @client.event
 async def on_ready():
-    return True
-    #await client.get_channel(782480148011679844).send("I'm ready!")
+    await client.get_channel(782480148011679844).send("I'm ready!")
     #await client.get_channel(782480148011679844).send(ex.re)
 
 client.run(botToken)
