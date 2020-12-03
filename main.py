e58c2a0db8e1cfaecef52da89b6b332dee93efba
WingsOfGold <61005991+WingsOfGold@users.noreply.github.com>
2020-12-03 12:18:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import exports.test as ex
 
 client = commands.Bot(command_prefix='*')#, intents=intents)
 botToken = "Nzc2NDAxMTQ3OTc1NTY1MzEz.X60V6g.yN4Y4wmj4RR2yGaXsbhSWUlUv7g"
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong? Ah! Ping: {round(client.latency *1000)}ms")
+
 @client.event
 async def on_ready():
     await client.get_channel(782480148011679844).send("I'm ready!")
